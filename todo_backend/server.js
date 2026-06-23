ad2d3b3de3f3c41c60a6da82d6a63e0d3a77d01b
Harshal Gaulkar <141926540+harshalgaulkar@users.noreply.github.com>
2026-06-23 15:51:38 +0530

    coneected the routes with the server page

--- todo_backend/server.js
+++ todo_backend/server.js
@@ -9,8 +9,12 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'token'],
     credentials: true
 }))
+
+const todoRoutes = require('./routes/todo')
+
+app.use('/api/todos', todoRoutes)
 app.use(express.json())
-    
+
 const port = process.PORT || 4000
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server started at port ${port}`)
